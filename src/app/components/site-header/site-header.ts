0f287b355bca96485b1d7955ece904f4623e7a98
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-06-03 00:02:34 +0200

    Adjust styling and create toggle menu

--- src/app/components/site-header/site-header.ts
+++ src/app/components/site-header/site-header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router'; 
 
 @Component({
@@ -7,4 +7,15 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './site-header.html',
   styleUrl: './site-header.scss',
 })
-export class SiteHeader {}
+export class SiteHeader {
+  menuOpen = signal(false);
+
+  toggleMenu(): void {
+    this.menuOpen.update(isOpen => !isOpen);
+  }
+
+  closeMenu(): void {
+    this.menuOpen.set(false);
+  }
+
+}
