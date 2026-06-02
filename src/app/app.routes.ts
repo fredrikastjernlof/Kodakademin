d9d06cd591f8ccd9fef2acb02125d38e19f580a8
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-06-02 18:53:41 +0200

    Create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Courses } from './pages/courses/courses';
+import { Schedule } from './pages/schedule/schedule';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'startsida', component: Home },
+  { path: 'kurser', component: Courses },
+  { path: 'ramschema', component: Schedule },
+  { path: "404", component: NotFound},
+  { path: "**", redirectTo: "404", pathMatch: "full"}
+];
