925a6bec8be1a56b186a28c11f333fbcf8e8977c
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-06-02 20:10:29 +0200

    Display course data from JSON using Angular signals

--- src/app/pages/courses/courses.ts
+++ src/app/pages/courses/courses.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CourseService } from '../../services/course.service';
 
 @Component({
   selector: 'app-courses',
@@ -6,4 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './courses.html',
   styleUrl: './courses.scss',
 })
-export class Courses {}
+export class Courses {
+
+  // Inject the CourseService using Angular's dependency injection
+  private courseService = inject(CourseService);
+
+  // Update courses when the service data changes
+  courses = this.courseService.allCourses;
+
+  // Load courses when the component is initialized
+  constructor() {
+    this.courseService.loadCourses();
+  }
+}
