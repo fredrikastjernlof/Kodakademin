046282929670b27260553bd84c54cc75c1c841fd
fredrikastjernlof <fredrika.stjernlof@live.se>
2026-06-04 06:07:04 +0200

    Add link and styling  to add new courses

--- src/app/pages/schedule/schedule.ts
+++ src/app/pages/schedule/schedule.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ScheduleService } from '../../services/schedule.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-schedule',
-  imports: [],
+  imports: [ RouterLink ],
   templateUrl: './schedule.html',
   styleUrl: './schedule.scss',
 })
